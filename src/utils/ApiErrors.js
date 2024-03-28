dc1b803b28207939674963108614cf92b8866ef5
mohitgaur310 <mohitgaur310@gmail.com>
2024-03-29 00:53:30 +0530

    configured the joi for validation

--- src/utils/ApiErrors.js
+++ src/utils/ApiErrors.js
@@ -1,21 +1,9 @@
 class ApiError extends Error {
-  constructor(
-    statusCode,
-    message = "somthing went wrong",
-    errors = [],
-    stack = "",
-  ) {
+  statusCode;
+  constructor(statusCode, message) {
     super(message);
     this.statusCode = statusCode;
-    this.data = null;
-    this.message = message;
-    this.success = false;
-    this.errors = errors;
-    if (stack) {
-      this.stack = stack;
-    } else {
-      Error.captureStackTrace(this, this.constructor);
-    }
+    Error.captureStackTrace(this, this.constructor);
   }
 }
 
