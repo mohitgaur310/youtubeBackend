4ee9299db297f7cdd1fb6afef62e9ed828f4bda5
mohitgaur310 <mohitgaur310@gmail.com>
2024-04-07 04:26:57 +0530

    bcrypt

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -17,8 +17,6 @@ const registerUser = asyncHandler(async (req, res, next) => {
     }
     const avatarLocalPath = req.files.avatar[0]?.path;
     const coverImageLocalPath = req.files.coverImage[0]?.path;
-    console.log("avatar==>", avatarLocalPath);
-    console.log("cover image==>", coverImageLocalPath);
 
     const avatar = await uploadOnCloudinary(avatarLocalPath);
     const coverImage = await uploadOnCloudinary(coverImageLocalPath);
